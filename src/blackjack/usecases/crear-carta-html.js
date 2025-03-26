6514853dd7a6506206b2f8d3e3d54c2e661f7560
Jose Gomez <jose.gomez.villasclaras@gmail.com>
2025-03-26 15:41:03 +0100

    Limpieza

--- src/blackjack/usecases/crear-carta-html.js
+++ src/blackjack/usecases/crear-carta-html.js
@@ -8,9 +8,9 @@ export const crearCartaHTML = ( carta ) => {
 
     if ( !carta ) throw new Error('La carta es un argumento obligatorio');
 
-    // <img class="carta" src="assets/cartas/2C.png">
+    
     const imgCarta = document.createElement('img');
-    imgCarta.src = `assets/cartas/${ carta }.png`; //3H, JD
+    imgCarta.src = `assets/cartas/${ carta }.png`; 
     imgCarta.classList.add('carta');
 
     return imgCarta;
